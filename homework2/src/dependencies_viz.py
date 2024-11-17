12651831dc4db47cf2a72901ae9a966e837c5103
mudra <mirina-0208@yandex.ru>
2024-11-17 21:21:35 +0300

    drawing graph funcs done

--- homework2/src/dependencies_viz.py
+++ homework2/src/dependencies_viz.py
@@ -1,7 +1,9 @@
-from typing import Dict, Optional, Callable
+from typing import Dict, Optional, Callable, List
 from langchain_core.runnables.graph_mermaid import draw_mermaid_png
+from python_mermaid.diagram import MermaidDiagram, Node, Link
 import json
 import requests
+from python_mermaid.link import Link
 
 
 def get_dependencies_current(package_name: str) -> Dict:
@@ -34,14 +36,46 @@ def get_all_dependencies(package_name: str, depth: int, dep_dict) -> Optional[Ca
     else:
         dep_dict[package_name] = packages
         for p in dep_dict[package_name]:
-            res = get_all_dependencies(p, depth-1, dep_dict)
+            res = get_all_dependencies(p, depth - 1, dep_dict)
 
             # если нет данных о зависимостях или превышена глубина - продолжаем перебирать зависимости на том же уровне
             if res is None:
                 continue
 
+
+def is_node_in_list(name: str, nodes: List) -> bool:
+    for n in nodes:
+        if n.content == name:
+            return True
+    return False
+
+
+def find_node_by_name(name: str, nodes: List) -> bool:
+    for n in nodes:
+        if n.content == name:
+            return n
+
+
 def get_mermaid_str(dependencies: Dict) -> str:
     """Создаёт строку разметки для графа"""
+    nodes: list[Node] = []
+    links: list[Link] = []
+
+    for package in dependencies.keys():
+        if not is_node_in_list(package, nodes):
+            nodes.append(Node(package))
+        parent = find_node_by_name(package, nodes)
+
+        deps = dependencies[package]
+        for dep in deps:
+            if not is_node_in_list(dep, nodes):
+                nodes.append(Node(dep))
+            child = find_node_by_name(dep, nodes)
+
+            links.append(Link(parent, child))
+
+    script = MermaidDiagram(title="Dependencies graph", nodes=nodes, links=links)
+    return str(script)
 
 
 def get_graph_png(mermaid_str: str, output_path: str) -> None:
@@ -58,12 +92,10 @@ def main():
     dependencies = {}
     get_all_dependencies(package_name=data["package_name"], depth=data["max_depth"], dep_dict=dependencies)
 
+    mermaid_script = get_mermaid_str(dependencies)
+    get_graph_png(mermaid_str=mermaid_script, output_path=data["graph_output_path"])
 
-    # mermaid_script = get_mermaid_str(dep_dict)
-    #
-    # get_graph_png(mermaid_script, data["graph_output_path"])
-    #
-    # print("Программа выполнилась без ошибок!")
+    print("Программа выполнилась без ошибок!")
 
 
 if __name__ == "__main__":
