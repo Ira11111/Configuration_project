3d5578ee32f717c5786cdd01060d505f60d68dbf
mudra <mirina-0208@yandex.ru>
2024-11-20 22:45:01 +0300

    graph picture program

--- homework2/src/get_graph.py
+++ homework2/src/get_graph.py
@@ -1,33 +1,19 @@
 import argparse
-import json
 import os
-import subprocess
-import shlex
 
 
 def get_graph_png(mermaid_path: str, output_file: str) -> None:
     """Преобразует строку разметки в изображение
     """
+    os.system(f"mmdc -i {mermaid_path} output -o {output_file}")
 
-    cmd = shlex.split(f"mmdc -i {mermaid_path} -o {output_file} -f png")
-    proc = subprocess.Popen(cmd)
-    proc.wait()
-
-    # if os.path.exists("mermaid.mmd"):
-    #     os.remove("mermaid.mmd")
-
-
-def main(mermaid_path: str):
-    with open("config/config.json", 'r') as conf_data:
-        data = json.load(conf_data)
-
-    output_file = data["graph_output_path"]
-    get_graph_png(mermaid_path, output_file)
+    if os.path.exists("src/mermaid.mmd"):
+        os.remove("src/mermaid.mmd")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Эмулятор оболочки")
     parser.add_argument("--mf", required=True, help="путь до файла с разметкой")
+    parser.add_argument("--of", required=True, help="путь до файла  с изображением")
     args = parser.parse_args()
-
-    main(args.mf)
+    get_graph_png(args.mf, args.of)
