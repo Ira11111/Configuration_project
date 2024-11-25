cae5251f7992dc2f71b6afefbb850bcd4aa71c7a
ira <mirina-0208@yandex.ru>
2024-11-25 21:10:15 +0300

    logic change

--- homework3/src/converter.py
+++ homework3/src/converter.py
@@ -1,84 +1,144 @@
-from lark import Lark, Transformer, v_args
+import ast
 import sys
 import yaml
-
-grammar = """
-    ?start: expr
-
-    ?expr: const_decl
-         | COMMENT
-         | COMMENTS
-
-    const_decl: "var " NAME " " value 
-
-    const_expr: "${" NAME "}"
-
-    array: "[" [value ("," value)*] "]"
-
-    ?value: NUMBER
-          | const_expr
-          | array
-    
-    COMMENT: "REM" /./
-    COMMENTS: "/+" /*/ "+/"
-    NAME: /[_a-z]+/
-    NUMBER: /[0-9]+/
-
-    %import common.WS
-    %import common.NL
-    %ignore WS
-    %ignore COMMENT
-    %ignore COMMENTS
-"""
+import re
 
 
-@v_args(inline=True)
-class TreeToDict(Transformer):
-    """Класс, который трансформирует дерево разбора в словарь python"""
-    from_lark = True
 
-    def const_decl(self, name, value):
-        return {name: value}
+class Parser:
+    def __init__(self, text):
+        self.text = text
+        self.res_dict = {}
 
-    def const_expr(self, name):
-        return name
+    def parse(self):
+        """Основная функция обработки"""
+        try:
+            self.delete_comments()
 
-    def array(self, *items):
-        return list(items)
+            self.find_all_constants()
+            self.constant_decl()
+            self.const_expr()
+            self.get_arrays()
 
-    def value(self, item):
-        return item
+            if self.check_text():
+                raise SyntaxError("В тексте языка были допущены синтаксические ошибки!")
 
-    def NUMBER(self, token):
-        return int(token)
+        except Exception as e:
+            return str(e)
+        else:
+            self.make_yaml()
 
-    def NAME(self, token):
-        return token.value
+    def delete_comments(self):
+        """Метод для удаления комментариев в тексте языка"""
+        comments = []
+        pattern1 = r"/\+(.*?)\+/"
+        pattern2 = r"REM(.*?)\n"
+        matches1 = re.findall(pattern1, self.text, re.DOTALL)
+        matches2 = re.findall(pattern2, self.text)
+        if matches1 is not None:
+            comments.extend(matches1)
+            while len(comments) > 0:
+                s = "/+" + comments.pop(0) + "+/"
+                self.text = self.text.replace(s, "")
+
+        if matches2 is not None:
+            comments.extend(matches2)
+            while len(comments) > 0:
+                s = "REM" + comments.pop(0)
+                self.text = self.text.replace(s, "")
+
+        if matches2 is not None:
+            comments.extend(matches2)
+
+    def find_all_constants(self):
+        pattern = r"var [_a-zA-Z]+ .+"
+        matches = re.findall(pattern, self.text)
+        for m in matches:
+            const = m.split(" ")[1]
+            self.res_dict[const] = None
+
+    def constant_decl(self):
+        """Метод, который сопоставляет объявленным константам значения-числа"""
+        pattern = r"var [_a-zA-Z]+ \d+"
+        matches = re.findall(pattern, self.text)
+        for m in matches:
+            const = m.split(" ")[1]
+            val = m.split(" ")[2]
+            if self.res_dict[const] is not None:
+                raise NameError("Переменная с таким именем была уже объявлена")
+            else:
+                self.text = self.text.replace(m, "")
+                self.res_dict[const] = int(val)
+
+    def const_expr(self):
+        """Метод, который сопоставляет значения констант со значениями объявленных констант"""
+        pattern = r"var [_a-zA-Z]+ \$\{[a-zA-Z_]+\}"
+        matches = re.findall(pattern, self.text)
+        for m in matches:
+            const = m.split(" ")[1]
+            val = m.split(" ")[2][2:-1]
+            if val in self.res_dict.keys():
+                self.text = self.text.replace(m, "")
+                self.res_dict[const] = self.res_dict[val]
+            else:
+                raise NameError("Переменная с таким именем не была объявлена")
+
+    def get_arrays(self):
+        """Метод, который ищет все массивы"""
+        pattern = r"var [_a-zA-Z]+ \[.*\]"
+        matches = re.findall(pattern, self.text)
+        for m in matches:
+            array_string = self.validate_values(m)
+            evaluated_list = list(ast.literal_eval(array_string))
+
+            const = m.split(" ")[1]
+            if self.res_dict[const] is None:
+                self.text = self.text.replace(m, "")
+                self.res_dict[const] = evaluated_list
+            else:
+                raise NameError("Переменная с таким именем была уже объявлена")
 
 
-parser = Lark(grammar)
+
+    def validate_values(self, m_string):
+        """Метод, который подставляет на место вычисляемых констант значения"""
+        array_string = m_string[m_string.index("["):]
+        if array_string.count("[") != array_string.count("]"):
+            raise SyntaxError(f"Некорректный синтаксис в массиве {array_string}")
+
+        val_pattern = r"\$\{[a-zA-Z_]+\}"
+        val_matches = re.findall(val_pattern, array_string)
+        for v_m in val_matches:
+            if v_m[2:-1] not in self.res_dict.keys():
+                raise NameError("Переменная с таким именем не была объявлена")
+            else:
+                value = self.res_dict[v_m[2:-1]]
+                array_string = array_string.replace(v_m, str(value))
+
+        return array_string
+
+    def check_text(self):
+        return self.text is None
+
+    def make_yaml(self):
+        yaml_string = yaml.dump(self.res_dict, default_flow_style=False)  # default_flow_style=False делает вывод более читаемым
+        return yaml_string
 
 
-# Функция для парсинга и обработки ошибок
-def parse_config(text: str):
-    try:
-        text = "".join(sys.stdin)
-        tree = parser.parse(text)
-        data = TreeToDict().transform(tree)
-        yaml_output = yaml.dump(data, default_flow_style=False, indent=2)
-        return yaml_output
-
-    except Exception as exp:
-        return f"Ошибка при обработке:\n{str(exp)}"
-
+# var d 4
+# var f ${d}
+# var t [1, 2, [${f}, 3], ${d}]
 
 if __name__ == "__main__":
-    output = ""
-    while True:
-        line = sys.stdin.readline()
-        if not line:  # Проверка на пустую строку (EOF)
-            break
-        output += line
-        print(f"Получена строка: {line.strip()}")
-        # Обработка строки line
-    print(output)
+
+    output = """var d 4\nvar f ${d}\nvar t [1, 2, [${f}, 3], ${d}] REM fds"""
+    # while True:
+    #     line = sys.stdin.readline()
+    #     if not line:
+    #         break
+    #     output += line
+
+    parser = Parser(output)
+    res = parser.parse()
+    print(parser.res_dict)
+    print(res)
